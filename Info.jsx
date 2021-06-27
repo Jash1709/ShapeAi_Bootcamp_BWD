9571e384c5ed26240c2c7ebf6b4686f644d47c31
Jash1709 <86555258+Jash1709@users.noreply.github.com>
2021-06-27 16:42:52 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>javascript and reactjs</h1>
+      <p>new javascript and reactjs bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
